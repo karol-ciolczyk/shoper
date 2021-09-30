b5c56695c2469c3efdd791c51507e17d15ae5fba
Karol Ciołczyk <dyweko@gmail.com>
2021-09-30 11:33:18 +0200

    bug in removeDomTaskElement fixed

--- modules/DOM-manipulation/removeDomTaskElement.js
+++ modules/DOM-manipulation/removeDomTaskElement.js
@@ -1,6 +1,7 @@
 export const removeDomTaskElement = function (id, queue) {
   if (id) {
-    const taskIndex = queue.findIndex((task) => task.taskId === id);
+    const taskIndex = queue.findIndex((task) => task.taskId === +id);
+    console.log(taskIndex);
     if (taskIndex > -1) {
       queue.splice(taskIndex, 1);
     }
